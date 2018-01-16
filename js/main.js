b92f0868230524da08be1e90abf74f62006a7fbb
Christine <kang.christine@gmail.com>
2018-01-15 17:56:13 -0800

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,19 +1,22 @@
 var cards = ['queen', 'queen', 'king', 'king'];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-var cardTwo = cards[2];
-
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
-
-if (cardsInPlay.length === 2) {
+var checkForMatch = function () {
   if (cardsInPlay[0] === cardsInPlay[1]) {
     alert('You found a match!');
   } else {
-    alert('Sorry, try again!');
+    alert('Sorry, try again.');
   }
 }
+
+var flipCard = function (cardId) {
+  var cardOne = cards[0];
+  var cardTwo = cards[2];
+
+  console.log('User flipped ' + cards[cardId]);
+  cardsInPlay.push(cards[cardId]);
+  checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
